6c51e9e36ef9cbcd0878061eed943f3a012baeaf
Peter Laggner <peter.laggner@student.tugraz.at>
2021-10-10 00:14:35 +0200

    finish usecase flow

--- core/src/main/java/com/greyhairredbear/racooin/core/usecase/CalculateBalancesUsecase.kt
+++ core/src/main/java/com/greyhairredbear/racooin/core/usecase/CalculateBalancesUsecase.kt
@@ -2,21 +2,20 @@ package com.greyhairredbear.racooin.core.usecase
 
 import arrow.core.Either
 import arrow.core.computations.either
+import com.greyhairredbear.racooin.core.businesslogic.toFiatBalances
 import com.greyhairredbear.racooin.core.interfaces.ApiClient
 import com.greyhairredbear.racooin.core.interfaces.Persistence
-import com.greyhairredbear.racooin.core.model.CryptoBalance
+import com.greyhairredbear.racooin.core.model.FiatBalance
 import com.greyhairredbear.racooin.core.model.UsecaseError
 
 
 suspend fun calculateBalances(
     apiClient: ApiClient,
     persistence: Persistence,
-): Either<UsecaseError, List<CryptoBalance>> =
+): Either<UsecaseError, List<FiatBalance>> =
     either {
         val rates = getCurrencyRates(apiClient, persistence).bind()
         val cryptoBalances = persistence.fetchAllCryptoBalances().bind()
-
-        // TODO: calc
-
-        listOf()
+        val result = cryptoBalances.toFiatBalances(rates).bind()
+        result
     }
