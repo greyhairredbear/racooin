8ccb98115f86c21c16964598791453524773f94c
Peter Laggner <peter.laggner@student.tugraz.at>
2021-11-04 23:39:28 +0100

    WIP ui list element

--- app/src/main/java/com/greyhairredbear/racooin/ui/MainScreen.kt
+++ app/src/main/java/com/greyhairredbear/racooin/ui/MainScreen.kt
@@ -1,18 +1,25 @@
 package com.greyhairredbear.racooin.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
-import com.greyhairredbear.racooin.core.model.CryptoFiatBalance
+import com.greyhairredbear.racooin.core.model.CryptoBalance
+import com.greyhairredbear.racooin.core.model.CryptoCurrency
+import com.greyhairredbear.racooin.core.model.FiatCurrency
 
 @Composable
 fun MainScreen(
@@ -55,4 +62,51 @@ fun CryptoCurrencySummary(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun CryptoCurrencyOverviewListElement(
+    @PreviewParameter(CryptoCurrencyOverviewUiModelProvider::class) uiState: CryptoCurrencyOverviewUiModel,
+// TODO: put into data class for this composable together with uiState to enable prieview
+//    onUpdatedBalance: (CryptoBalance) -> Unit,
+) {
+    Column {
+        Row {
+            Text(text = uiState.cryptoCurrency.name)
+        }
+        Row {
+            TextField(
+                value = uiState.cryptoBalance.toString(),
+                onValueChange = {},
+            )
+        }
+        Row {
+            Text(text = "Balance")
+            Text(text = uiState.fiatBalance.toString() + uiState.fiatCurrency.name)
+        }
+        Row {
+            Text(text = "Rate")
+            Text(text = uiState.rate.toString() + uiState.fiatCurrency.name)
+        }
+    }
+}
+
+class UpdatedBalanceProvider : PreviewParameterProvider<(CryptoBalance) -> Unit> {
+    override val values = sequenceOf<(CryptoBalance) -> Unit>({})
+
+}
+
+class CryptoCurrencyOverviewUiModelProvider :
+    PreviewParameterProvider<CryptoCurrencyOverviewUiModel> {
+    override val values = sequenceOf(
+        CryptoCurrencyOverviewUiModel(
+            CryptoCurrency.ETHEREUM,
+            FiatCurrency.EURO,
+            rate = 2452.86,
+            cryptoBalance = 1.02,
+            fiatBalance = 2720.08
+        )
+    )
+
 }
